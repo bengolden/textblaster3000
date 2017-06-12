5f23f1ecd66bcb4f3ca4a95930714b83223d228e
Ben Golden <bengolden1@gmail.com>
2017-06-12 09:32:48 -0500

    remove replies routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blasts, only: [:new, :create]
   resources :responses, only: :index
-  resources :replies, only: [:new, :create]
   resource :tos, only: :show
   resource :authorize, only: :show
   resource :deauthorize, only: :show
