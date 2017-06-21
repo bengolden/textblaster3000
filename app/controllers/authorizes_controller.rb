85dd87afac8e9a9fd7b15f65ecee3873a52f0dfc
Ben Golden <bengolden1@gmail.com>
2017-06-21 11:55:13 -0500

    debuggin

--- app/controllers/authorizes_controller.rb
+++ app/controllers/authorizes_controller.rb
@@ -1,7 +1,7 @@
 class AuthorizesController < ApplicationController
   def show
     sid = params['AccountSid']
-    client = Twilio::REST::Client.new account_sid, auth_token
+    client = Twilio::REST::Client.new sid, Rails.application.secrets.twilio_auth_token
     session[:sid] = client.accounts.list.find{|acc| acc.incoming_phone_numbers.list.length > 0}&.sid
     redirect_to root_path
   end
