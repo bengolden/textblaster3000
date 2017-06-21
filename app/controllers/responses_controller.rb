edf8e127ea67d2860e4bd28cd98d46abcd21d2f2
Ben Golden <bengolden1@gmail.com>
2017-06-21 12:16:00 -0500

    debug

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,12 +1,12 @@
 class ResponsesController < ApplicationController
 
   def index
-    @messages = []
+    @responses = []
     if session[:sid].present?
       auth_token = Rails.application.secrets.twilio_auth_token
       client = Twilio::REST::Client.new(session[:sid], auth_token)
       phone_numbers = client.incoming_phone_numbers.list.map(&:phone_number)
-      @messages = client.messages.list.select{ |m| !phone_numbers.include? m.from }
+      @responses = client.messages.list.select{ |m| !phone_numbers.include? m.from }
     end
   end
 
