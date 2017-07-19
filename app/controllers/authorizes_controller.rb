d9f72f349845f935a013f42dd12c6f812f791d3e
Ben Golden <bengolden1@gmail.com>
2017-07-19 15:52:37 -0500

    debugging

--- app/controllers/authorizes_controller.rb
+++ app/controllers/authorizes_controller.rb
@@ -4,11 +4,12 @@ class AuthorizesController < ApplicationController
     purchase_phone_number_if_needed
     redirect_to root_path
   end
+  
 
   def purchase_phone_number_if_needed
-    client = Twilio::REST::Client.new session[:sid], Rails.application.secrets.twilio_auth_token
-    return if client.account.incoming_phone_numbers.list.length > 0
-    number = client.available_phone_numbers.list.select{|c| c.country == "United States"}.first.local.list.first.phone_number
-    client.incoming_phone_numbers.create(phone_number: number)
+    # client = Twilio::REST::Client.new session[:sid], Rails.application.secrets.twilio_auth_token
+    # return if client.account.incoming_phone_numbers.list.length > 0
+    # number = client.available_phone_numbers.list.select{|c| c.country == "United States"}.first.local.list.first.phone_number
+    # client.incoming_phone_numbers.create(phone_number: number)
   end
 end
