96ec2ed9a2d6886767086d2adebd100793329295
Ben Golden <bengolden1@gmail.com>
2017-07-19 14:59:35 -0500

    add logger

--- app/controllers/authorizes_controller.rb
+++ app/controllers/authorizes_controller.rb
@@ -1,6 +1,7 @@
 class AuthorizesController < ApplicationController
   def show
     sid = params['AccountSid']
+    logger.info params.inspect
     client = Twilio::REST::Client.new sid, Rails.application.secrets.twilio_auth_token
     session[:sid] = client.accounts.list.find{|acc| acc.incoming_phone_numbers.list.length > 0}&.sid
     redirect_to root_path
