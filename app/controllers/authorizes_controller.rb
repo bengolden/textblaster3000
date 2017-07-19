1e0b93f6a38fc27aace6f0e526bba817c8df5a3a
Ben Golden <bengolden1@gmail.com>
2017-07-19 16:35:12 -0500

    set url for phone number on creation

--- app/controllers/authorizes_controller.rb
+++ app/controllers/authorizes_controller.rb
@@ -9,8 +9,14 @@ class AuthorizesController < ApplicationController
 
   def purchase_phone_number_if_needed
     client = Twilio::REST::Client.new session[:sid], auth_token
-    return if client.account.incoming_phone_numbers.list.length > 0
-    number = client.available_phone_numbers.list.select{|c| c.country == "United States"}.first.local.list.first.phone_number
-    client.incoming_phone_numbers.create(phone_number: number)
+    if client.account.incoming_phone_numbers.list.length == 0
+      number = client.available_phone_numbers.list.select{|c| c.country == "United States"}.first.local.list.first.phone_number
+      client.incoming_phone_numbers.create(phone_number: number)
+    end
+
+    phone_number = client.incoming_phone_numbers.list.first
+    if phone_number.sms_url.blank?
+      phone_number.update(sms_url: "http://textblaster3000.herokuapp.com/tos", sms_method: "GET")
+    end
   end
 end
