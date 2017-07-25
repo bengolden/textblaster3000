568ed0b7059c3a0dd4d0fd7c67f72b625dbbe47b
Ben Golden <bengolden1@gmail.com>
2017-07-25 12:03:55 -0500

    bugfix

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -3,12 +3,12 @@ class PhoneNumbersController < ApplicationController
   def new
     @phone_number_count = current_client.account.incoming_phone_numbers.list.length
     logger.info params.inspect
-    if !params[:country]
+    if !permitted_params[:country]
       @countries = available_phone_numbers.map(&:country)
-    elsif !params[:area_code]
+    elsif !permitted_params[:area_code]
       @select_area_code = true
     else
-      @available_numbers = available_phone_numbers.find{|c| c.country == "United States"}.local.list(area_code: params[:area_code]).map(&:phone_number)
+      @available_numbers = available_phone_numbers.find{|c| c.country == "United States"}.local.list(area_code: permitted_params[:area_code]).map(&:phone_number)
     end
   end
 
