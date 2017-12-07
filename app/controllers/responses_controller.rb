0cf1857319d999d479a6c5a77aad961a94dfdd33
Ben Golden <bengolden1@gmail.com>
2017-12-07 11:09:51 -0600

    tidyup

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,7 +4,9 @@ class ResponsesController < ApplicationController
     @responses = []
     if session[:sid].present?
       phone_numbers = current_client.incoming_phone_numbers.list.map(&:phone_number)
-      @responses = current_client.messages.list.select{ |m| !phone_numbers.include? m.from }.map{|r| {from: r.from, body: r.body, sent: r.date_sent}}
+      @responses = current_client.messages.list.reject{|m| phone_numbers.include? m.from }.map do |r|
+        {from: r.from, body: r.body, sent: r.date_sent}
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blasts, only: [:new, :create]
   resources :responses, only: :index
   resources :phone_numbers, only: [:new, :create]
